04f95137e832ead411decf6edd27dc294f8c0dfd
kazi-akib-abdullah <q.aqib.abdullah@gmail.com>
2022-10-25 08:33:42 +0600

    updated

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,75 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+      {
+        id: 1,
+        title: "Wake Up",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "Get Bananas",
+        done: false,
+      },
+      {
+        id: 3,
+        title: "Eat Bananas",
+        done: false,
+      },
+    ],
+    snackbar: {
+      show: false,
+      text: ''
+    }
   },
   getters: {
   },
   mutations: {
+    addTask(state, newTaskTitle) {
+      let newTask = {
+        id: Date.now(),
+        title: newTaskTitle,
+        done: false,
+      };
+      state.tasks.push(newTask);
+      // this.newTaskTitle = "";
+    },
+    doneTask(state, id) {
+      let task = state.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(state, id) {
+      state.tasks = state.tasks.filter((task) => task.id !== id);
+    },
+    updateTaskTitle(state, payload) {
+      let task = state.tasks.filter((task) => task.id === payload.id)[0];
+      task.title = payload.title
+    },
+    showSnackbar(state, text) {
+      let timeout = 0;
+      if (state.snackbar.show) {
+        state.snackbar.show = false
+        timeout = 200;
+      }
+      setTimeout(() => {
+        state.snackbar.show = true
+        state.snackbar.text = text
+      }, timeout);
+    },
+    hideSnackbar(state) {
+      state.snackbar.show = false
+    }
   },
   actions: {
+    addTask({ commit }, newTaskTitle) {
+      commit('addTask', newTaskTitle);
+      commit('showSnackbar', 'Task Added!');
+    },
+    deleteTask({ commit }, id) {
+      commit('deleteTask', id)
+      commit('showSnackbar', 'Task Deleted!')
+    }
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consectetur
+      voluptas sit labore suscipit voluptatum. Voluptatum quos exercitationem
+      porro totam, ipsum ex nulla facere molestiae quas voluptates vero
+      consequuntur quod.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consectetur
+      voluptas sit labore suscipit voluptatum. Voluptatum quos exercitationem
+      porro totam, ipsum ex nulla facere molestiae quas voluptates vero
+      consequuntur quod.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consectetur
+      voluptas sit labore suscipit voluptatum. Voluptatum quos exercitationem
+      porro totam, ipsum ex nulla facere molestiae quas voluptates vero
+      consequuntur quod.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consectetur
+      voluptas sit labore suscipit voluptatum. Voluptatum quos exercitationem
+      porro totam, ipsum ex nulla facere molestiae quas voluptates vero
+      consequuntur quod.
+    </p>
   </div>
 </template>
